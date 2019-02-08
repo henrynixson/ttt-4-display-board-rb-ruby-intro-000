6346b63ef910a0b219f53a0eba6cdf1fe3600c5e
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-08 20:19:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,5 @@
 board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
 
 def display_board(board)
-  puts "#{board[0]}|, #{board[1]}|, #{board[]}"
+  puts "#{board[0]}|, #{board[1]}|, #{board[2]}"
 end
