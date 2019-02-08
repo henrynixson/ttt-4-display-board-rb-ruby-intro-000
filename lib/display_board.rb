b131cd532badf7384f3565ba95ce86f0f5fed644
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-08 20:14:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
 def display_board(board)
-puts ""
+  puts ""
 end
